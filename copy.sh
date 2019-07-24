a365cef6a144749d1a9815d9d09cc02f7b7b3768
RSyiets <ravy.a.turns@gmail.com>
2019-07-24 13:18:18 +0900

    copy.sh does not delete .shells/local.

--- copy.sh
+++ copy.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-rm -fr ~/.shells/
+rm -f ~/.shells/alias
+rm -f ~/.shells/git
+rm -f ~/.shells/print0
 rm -f ~/.bashrc
 rm -f ~/.vimrc
 rm -f ~/.vim_cheatsheet
 
-cp -r shells ~/.shells/
+cp shells/* ~/.shells/
 cp bashrc ~/.bashrc
 cp vimrc ~/.vimrc
 cp vim_cheatsheet ~/.vim_cheatsheet
